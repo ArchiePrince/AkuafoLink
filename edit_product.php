ee3bdda6ff814f222a93cc6016bc1de86c2d43ae
Jason-bolt <appiatujason@gmail.com>
2020-12-08 11:01:55 +0000

    Edit profile

--- edit_product.php
+++ edit_product.php
@@ -17,21 +17,30 @@
       <div class="row">
         <div class="col-lg-4 col-sm-6 mb-4">
           <div class="card h-10">
-            <a href="#"><img class="card-img-top" src="assets/img/farm_produce_0.1.png" alt="Farm produce"></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                Name of product
-                <!-- <a href="#">Project One</a> -->
-              </h4>
-              <p class="card-text"><strong>Seller's name:</strong> John Doe</p>
-              <p class="card-text"><strong>Location:</strong> Accra</p>
-              <p class="card-text"><strong>Product description:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
-              <p class="card-text"><strong>Available:</strong> Now</p>
-              <p class="card-text"><strong>Quantity:</strong> 10 sacs</p>
-              <!-- <p class="card-text text-center">Farm produce</p> -->
-            </div>
+            <form class="form-row" enctype="multipart/form-data">
+                <a href="#"><img class="card-img-top" src="assets/img/farm_produce_0.1.png" alt="Farm produce"></a>
+            
+              <div class="card-body">
+                <h4 class="card-title">
+                  <input type="text" name="product_name" class="form-control" placeholder="Name of product">
+                  <!-- <a href="#">Project One</a> -->
+                </h4>
+                <p class="card-text"><strong>Seller's name:</strong> John Doe</p>
+                
+                <div class="row container">
+                  <p class="card-text"><strong class="col">Location:</strong>
+                  <input type="text" class="col form-control" name="location" placeholder="Accra"></p> 
+                </div>
+                
+                <p class="card-text"><strong>Product description:</strong> 
+                  <textarea class="form-control" name="description"></textarea>
+                </p>
+                <p class="card-text"><strong>Available:</strong> <input class="form-control" value="10" type="number" name="available"></p>
+                <p class="card-text"><strong>Quantity:</strong> <input class="form-control" value="e.g. 10 sacs, 20 pieces, ..." type="text" name="quantity"></p>
+                <!-- <p class="card-text text-center">Farm produce</p> -->
+                <input type="submit" class="btn btn-success" name="edit_profile_submit" value="Save">
+              </div>
+            </form>    
           </div>
         </div>
 
